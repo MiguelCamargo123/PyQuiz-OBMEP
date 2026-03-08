a2acc432128b56e73454a0f4aa979d7118d9bb1b
Miguel Camargo <omiguelcamargo@gmail.com>
2026-03-08 16:43:17 -0300

    Metodo de carregar e criar historico feitos

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import random
 class Quiz:
     def __init__(self):
         self.questoes = []
+        self.historico = []
         self.acertos = 0
         self.erros = 0
+        self.perguntas_feitas = 0
         self._carregar_questoes()
 
     def _carregar_questoes(self):
@@ -34,6 +36,28 @@ class Quiz:
             if r['id'] == questao['id']:
                 if resposta_pessoa == r['resposta']:
                     print('Parabens, você acertou a questão!')
+                    self.acertos += 1
                 else:
                     print('Infelizmente, você errou a questão')
+                    self.erros += 1
                 break
+            self.perguntas_feitas += 1
+
+    def _carregar_historico(self):
+        try:
+            with open('historico.json', 'r', encoding='utf=8') as his:
+                self.historico = json.load(his)
+        except FileNotFoundError:
+            self.historico = []
+
+    def _criar_historico(self):
+        self.historico.append(
+            {
+                'Acertos': self.acertos,
+                'Erros': self.erros,
+                'Questões feitas': self.perguntas_feitas,
+            }
+        )
+
+        with open('historico.json', 'w', encoding='utf-8') as his:
+            json.dump(self.historico, his, ensure_ascii=False, indent=4)
